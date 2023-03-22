ce2050c0baf20121316cbb043cd7932c7901c34a
nedoletoff <nedoletoff@ya.ru>
2023-03-22 14:52:58 +0300

    make all function but decomposition isnot the same as the brute force

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-import numpy as np
+
+
+def decomposition(p):
+    return p + pow(p, 3) + pow(p, 5)
+    # return 2 * pow(p, 2) + 2 * pow(p, 3) + pow(p, 4) - pow(p, 5)
+
+
+def find_all_paths(graph, start_t, end_t, path=None):
+    if path is None:
+        path = []
+    path = path + [start_t]
+    if start_t == end_t:
+        return [path]
+    if not graph.has_node(start_t):
+        return []
+    paths = []
+    for node in graph[start_t]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end_t, path)
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
+
 
 with open("edges.txt") as f:
     lines = f.readlines()
@@ -13,30 +35,17 @@ g.add_edges_from(edgeList)
 nodes = g.nodes
 pos = nx.planar_layout(g)
 nx.draw(g, pos, with_labels=True, node_color="#f86e00")
+pr_all = [0] * 11
 
-tempg = g.copy()
-probs = np.linspace(0, 1, 11)
-bf_res = [0] * 10
+print(start, end)
+probs = (0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
+for i, prob in enumerate(probs):
+    paths = find_all_paths(g, start, end)
+    for path in paths:
+        pr_all[i] += pow(prob, len(path) - 1)
 
-dfs = nx.dfs_edges(g, source=start, depth_limit=1)
-count = 0
-while len(tempg) >= 1:
-    count += 1
-    for i in dfs:
-        if i[1] == end:
-            for bf, prob in zip(bf_res, probs):
-                bf += prob ** count
-    tempg.remove_node(dfs[0][0])
-    dfs = nx.dfs_edges(tempg, source=dfs[1], depth_limit=1)
-
-
-#for i in nx.bfs_layers(g, sources=start):
-
-
-print(start)
-print(end)
-print(bf_res)
-
-#nx.draw(bfs, pos, with_labels=True, node_color="#f86e00", edge_color="#dd2222")
+for i, prob in enumerate(probs):
+    print(f'probability = {prob}: probability decomposition ='
+          f' {decomposition(prob)}, probability brute force = {pr_all[i]}')
 
 plt.show()
